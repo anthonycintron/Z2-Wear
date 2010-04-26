5ad3b27721f6f3430222f0e0def2a92b9d1c0642
anthony <anthony.cintron@gmail.com>
2010-04-25 20:11:35 -0400

    implemented product save functionality

--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,21 @@
 Cufon.replace('h4');
 Cufon.replace('a');
 Cufon.now();
+
+
+
+$(document).ready(function(){
+	
+	//animate product box
+	$('#product-window').animate({
+    top: '+=485px'
+  }, 1000, function() {
+    // Animation complete.
+  });
+	
+	
+	
+	
+	
+	
+});
--- zwear.rb
+++ zwear.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'dm-core'
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/zwear.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/db/zwear.db")
 
 class Product
   
@@ -13,6 +13,10 @@ class Product
   property  :thumb,         String
   property  :large,         String
   property  :description,   Text
+  property  :largeImgName,  String
+  property  :thumbImgName,  String
+  property  :filename,      String
+  property  :size,          Integer
   property  :created_at,    DateTime
   property  :updated_at,    DateTime
   property  :content_type,  String
@@ -38,6 +42,9 @@ end
 
 # retrieve list of products
 get '/list' do
+  @product = "List Products"
+  @product = Product.all(:order => [:created_at.desc])
+  erb :list
 end
 
 get '/new' do
@@ -45,14 +52,36 @@ get '/new' do
   erb :new
 end
 
-get '/create' do
+post '/create' do
+  @product = Product.new(params[:product])
+  @product.largeImgName = params[:product][:large][:filename]
+  @product.thumbImgName = params[:product][:thumb][:filename]
+  if @product.save
+    # upload thumb image
+    thumbPath = File.join(Dir.pwd, "/public/products", @product.thumbImgName)
+    puts "THUMB IMAGE INFO: #{params[:product][:thumb][:filename]}"
+    File.open(thumbPath, "wb") do |f|
+      f.write(params[:product][:thumb][:tempfile].read)
+    end
+    # upload large image
+    largePath = File.join(Dir.pwd, "/public/products", @product.largeImgName)
+    File.open(largePath, "wb") do |f|
+      f.write(params[:product][:large][:tempfile].read)
+    end
+    redirect "/show/#{@product.id}"
+  else
+    redirect('/list')
+  end
 end
 
 get '/delete' do
 end
 
-get '/product/:name' do
+get '/show/:id' do
+  @product = Product.get(params[:id])
+  if @product
+    erb :show
+  else
+    redirect('/list')
+  end
 end
-
-
-
